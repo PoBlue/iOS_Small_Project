fd06460bd982854a314dd7a4a1c6deb9aba2bf04
blues <blues@blues.com>
2016-03-29 18:38:15 +0800

    fix

---
+++ 瀑布流/pinterest/AnnotatedPhotoCell.swift
@@ -0,0 +1,34 @@
+//
+//  AnnotatedPhotoCell.swift
+//  RWDevCon
+//
+//  Created by Mic Pringle on 26/02/2015.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class AnnotatedPhotoCell: UICollectionViewCell {
+  
+  @IBOutlet private weak var imageView: UIImageView!
+  @IBOutlet private weak var imageViewHeightLayoutConstraint: NSLayoutConstraint!
+  @IBOutlet private weak var captionLabel: UILabel!
+  @IBOutlet private weak var commentLabel: UILabel!
+  
+  var photo: Photo? {
+    didSet {
+      if let photo = photo {
+        imageView.image = photo.image
+        captionLabel.text = photo.caption
+        commentLabel.text = photo.comment
+      }
+    }
+  }
+  
+  override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes!) {
+    super.applyLayoutAttributes(layoutAttributes)
+    if let attributes = layoutAttributes as? PinterestLayoutAttributes {
+      imageViewHeightLayoutConstraint.constant = attributes.photoHeight
+    }
+  }
+}
